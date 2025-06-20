3bd624af0834276ab46217df2c4ac85052bfcf24
Ho Tan Hung <tanhungho2002@gmail.com>
2025-06-20 16:44:35 +0700

    feat: refactor DetailProduct component to be an async function for future data fetching

--- src/app/detail/[slug]/page.tsx
+++ src/app/detail/[slug]/page.tsx
@@ -5,17 +5,19 @@ interface DetailProductProps {
     slug: string;
   };
 }
-function DetailProduct({ params }: DetailProductProps) {
+
+// ✅ Phải là async function (dù bạn không cần fetch gì cả)
+const DetailProduct = async ({ params }: DetailProductProps) => {
   return (
     <div className="grid grid-cols-12 gap-6">
       <div className="col-span-3 hidden lg:block">
         <Categories />
       </div>
-      <div className="col-span-9  mt-2">
+      <div className="col-span-9 mt-2">
         <textarea value={params.slug} readOnly className="border-2 border-gray-300 w-full h-full" />
       </div>
     </div>
   );
-}
+};
 
 export default DetailProduct;
