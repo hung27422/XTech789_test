b05e5f3e96bcf0af46cee989932f3193389d0b5d
Ho Tan Hung <tanhungho2002@gmail.com>
2025-06-20 16:51:26 +0700

    refactor: simplify DetailProduct component by removing interface and using async function for params

--- src/app/detail/[slug]/page.tsx
+++ src/app/detail/[slug]/page.tsx
@@ -1,20 +1,17 @@
 import Categories from "@/components/layouts/categories";
 
-interface DetailProductProps {
-  params: { slug: string };
-}
-
-function DetailProduct({ params }: DetailProductProps) {
+const DetailProduct = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const { slug } = await params;
   return (
     <div className="grid grid-cols-12 gap-6">
       <div className="col-span-3 hidden lg:block">
         <Categories />
       </div>
       <div className="col-span-9 mt-2">
-        <textarea value={params.slug} readOnly className="border-2 border-gray-300 w-full h-full" />
+        <textarea value={slug} readOnly className="border-2 border-gray-300 w-full h-full" />
       </div>
     </div>
   );
-}
+};
 
 export default DetailProduct;
